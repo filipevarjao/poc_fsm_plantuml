946a8df81807198147c15afad95699bfefc00028
Filipe Varjão <varjaofilipe@gmail.com>
2018-12-18 16:12:07 -0300

    first commit

---
+++ lib/poc_fsm_plantuml.ex
@@ -0,0 +1,18 @@
+defmodule PocFsmPlantuml do
+  @moduledoc """
+  Documentation for PocFsmPlantuml.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PocFsmPlantuml.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/poc_fsm_plantuml_test.exs
@@ -0,0 +1,8 @@
+defmodule PocFsmPlantumlTest do
+  use ExUnit.Case
+  doctest PocFsmPlantuml
+
+  test "greets the world" do
+    assert PocFsmPlantuml.hello() == :world
+  end
+end
