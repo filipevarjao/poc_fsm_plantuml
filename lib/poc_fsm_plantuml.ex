6193fae31968ce53105d1d27ff743be6b47ba089
Filipe Varjão <varjaofilipe@gmail.com>
2018-12-19 17:25:13 -0300

    starting parse plantuml

--- lib/poc_fsm_plantuml.ex
+++ lib/poc_fsm_plantuml.ex
@@ -15,4 +15,15 @@ defmodule PocFsmPlantuml do
   def hello do
     :world
   end
+
+  @doc """
+  We are assuming the plantuml file is correct.
+  """
+  def parse_plantuml(input) do
+    puml = input
+    |> String.split("\n")
+    |> Enum.map(fn x -> String.split(x, " ") end)
+
+    for i <- puml, i != [""], do: List.to_tuple(i)
+  end
 end
--- test/poc_fsm_plantuml_test.exs
+++ test/poc_fsm_plantuml_test.exs
@@ -5,4 +5,22 @@ defmodule PocFsmPlantumlTest do
   test "greets the world" do
     assert PocFsmPlantuml.hello() == :world
   end
+
+  test "parse a simple plantuml" do
+    content = """
+    @startuml
+    [*] --> State1
+    State1 --> [*]
+    @enduml
+    """
+
+    result = PocFsmPlantuml.parse_plantuml(content)
+
+    assert result == [
+      {"@startuml"},
+      {"[*]", "-->", "State1"},
+      {"State1", "-->", "[*]"},
+      {"@enduml"}
+    ]
+  end
 end
